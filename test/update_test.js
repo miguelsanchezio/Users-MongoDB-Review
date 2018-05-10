dc0a03ed3f34c0c0c8ec1cef802c325ac15a7ecf
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-05-09 22:09:34 -0700

    Wrote the remaining tests for the update test.

--- test/update_test.js
+++ test/update_test.js
@@ -8,15 +8,44 @@ describe('Updating records', () => {
     user.save()
       .then(() => done());
   });
-
-  it('instance type using set and save', done => {
-    user.set('name', 'Alex');
-    user.save()
+  
+  function assertName(operation, done) {
+    operation
       .then(() => User.find({}))
       .then(foundUsers => {
         assert(foundUsers.length === 1);
         assert(foundUsers[0].name === 'Alex');
         done();
       });
+  }
+
+  it('a model instance can set and save', done => {
+    user.set('name', 'Alex');
+    assertName(user.save(), done);
+  });
+
+  it('a model instance can update', done => {
+    assertName(user.update({ name: 'Alex' }), done);
+  });
+
+  it('a model class can update', done => {
+    assertName(
+      User.update({ name: 'Joe' }, { name: 'Alex' }),
+      done
+    );
+  });
+  
+  it('a model class can update one record', done => {
+    assertName(
+      User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex'}),
+      done
+    );
+  });
+
+  it('a model class can find a record with an id and update', done => {
+    assertName(
+      User.findByIdAndUpdate(user._id, { name: 'Alex'}),
+      done
+    );
   });
 });
